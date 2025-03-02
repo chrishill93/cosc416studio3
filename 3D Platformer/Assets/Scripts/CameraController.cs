8a8a3828122d8365dbf72395613bfcd97e3485d3
chrishill93 <chrishill_12@hotmail.com>
2025-03-02 10:13:15 -0800

    fixed Camera

--- 3D Platformer/Assets/Scripts/CameraController.cs
+++ 3D Platformer/Assets/Scripts/CameraController.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset;
-    public float rotationSpeed = 100f;
+    public Vector3 offset = new Vector3(0, 4, -8);
+    public float rotationSpeed = 500f;
 
     private float currentRotationX = 0f;
     private float currentRotationY = 0f;
@@ -15,10 +15,12 @@ public class CameraController : MonoBehaviour
 
         currentRotationX += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
         currentRotationY += Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
-        currentRotationY += Mathf.Clamp(currentRotationY, -40f, 60f);
+        currentRotationY = Mathf.Clamp(currentRotationY, -80f, -30f);
 
         Quaternion rotation = Quaternion.Euler(currentRotationY, currentRotationX, 0f);
-        transform.position = target.position - (rotation * offset);
+        Vector3 desiredPosition = target.position - (rotation * offset);
+
+        transform.position = desiredPosition;
         transform.LookAt(target.position);
     }
 }
