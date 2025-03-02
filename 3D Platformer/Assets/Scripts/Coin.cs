6bd570639d44083d279926125bce8d195ae59e89
chrishill93 <chrishill_12@hotmail.com>
2025-03-02 09:55:01 -0800

    created Coin script

--- 3D Platformer/Assets/Scripts/Coin.cs
+++ 3D Platformer/Assets/Scripts/Coin.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float rotationSpeed = 50f;
+    public int coinValue = 1;
+
+    private void Update()
     {
-        
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            GameManager.Instance.AddScore(coinValue);
+            Destroy(gameObject);
+        }
     }
 }
