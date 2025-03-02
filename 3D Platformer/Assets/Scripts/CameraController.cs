b1f51ad1120b98359ec9188926c94120fb723c9b
chrishill93 <chrishill_12@hotmail.com>
2025-03-02 09:42:02 -0800

    created CameraController script

--- 3D Platformer/Assets/Scripts/CameraController.cs
+++ 3D Platformer/Assets/Scripts/CameraController.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 
-public class CameraControllers : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform target;
+    public Vector3 offset;
+    public float rotationSpeed = 100f;
 
-    // Update is called once per frame
-    void Update()
+    private float currentRotationX = 0f;
+    private float currentRotationY = 0f;
+
+    private void LateUpdate()
     {
-        
+        if (!target) return;
+
+        currentRotationX += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+        currentRotationY += Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+        currentRotationY += Mathf.Clamp(currentRotationY, -40f, 60f);
+
+        Quaternion rotation = Quaternion.Euler(currentRotationY, currentRotationX, 0f);
+        transform.position = target.position - (rotation * offset);
+        transform.LookAt(target.position);
     }
 }
